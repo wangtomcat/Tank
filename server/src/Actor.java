0d0661665c9b5829af6ffa7115af83054f371197
王志源 <46014950+wangtomcat@users.noreply.github.com>
2019-05-31 20:57:27 +0800

    Add files via upload

---
+++ server/src/Actor.java
@@ -0,0 +1,11 @@
+//创建接口
+import java.awt.*;
+
+public interface Actor{
+	public void draw(Graphics g);
+	public void move();
+	public String getType();
+	public Rectangle getBorder();
+	public Rectangle[] getDetailedBorder();
+	public boolean walldestoried();
+}
